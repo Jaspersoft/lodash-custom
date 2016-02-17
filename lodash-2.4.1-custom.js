cdefd1da12521f620503d2b0d2d0061069cc3cac
taras42 <taras.my.mail@gmail.com>
2016-02-17 15:48:54 +0200

    Added constant and transform functions.

--- lodash-2.4.1-custom.js
+++ lodash-2.4.1-custom.js
@@ -1,7 +1,7 @@
 /**
  * @license
  * Lo-Dash 2.4.1 (Custom Build) <http://lodash.com/>
- * Build: `lodash underscore plus="cloneDeep,merge" exports="amd,commonjs,global,node"`
+ * Build: `lodash underscore exports="amd,commonjs,global,node" plus="cloneDeep,merge,transform,constant"`
  * Copyright 2012-2013 The Dojo Foundation <http://dojofoundation.org/>
  * Based on Underscore.js 1.5.2 <http://underscorejs.org/LICENSE>
  * Copyright 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
@@ -2127,6 +2127,58 @@
       }
     }
     return result;
+  }
+
+  /**
+   * An alternative to `_.reduce` this method transforms `object` to a new
+   * `accumulator` object which is the result of running each of its own
+   * enumerable properties through a callback, with each callback execution
+   * potentially mutating the `accumulator` object. The callback is bound to
+   * `thisArg` and invoked with four arguments; (accumulator, value, key, object).
+   * Callbacks may exit iteration early by explicitly returning `false`.
+   *
+   * @static
+   * @memberOf _
+   * @category Objects
+   * @param {Array|Object} object The object to iterate over.
+   * @param {Function} [callback=identity] The function called per iteration.
+   * @param {*} [accumulator] The custom accumulator value.
+   * @param {*} [thisArg] The `this` binding of `callback`.
+   * @returns {*} Returns the accumulated value.
+   * @example
+   *
+   * var squares = _.transform([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], function(result, num) {
+   *   num *= num;
+   *   if (num % 2) {
+   *     return result.push(num) < 3;
+   *   }
+   * });
+   * // => [1, 9, 25]
+   *
+   * var mapped = _.transform({ 'a': 1, 'b': 2, 'c': 3 }, function(result, num, key) {
+   *   result[key] = num * 3;
+   * });
+   * // => { 'a': 3, 'b': 6, 'c': 9 }
+   */
+  function transform(object, callback, accumulator, thisArg) {
+    var isArr = isArray(object);
+    if (accumulator == null) {
+      if (isArr) {
+        accumulator = [];
+      } else {
+        var ctor = object && object.constructor,
+            proto = ctor && ctor.prototype;
+
+        accumulator = baseCreate(proto);
+      }
+    }
+    if (callback) {
+      callback = createCallback(callback, thisArg, 4);
+      (isArr ? forEach : forOwn)(object, function(value, index, object) {
+        return callback(accumulator, value, index, object) === false && indicatorObject;
+      });
+    }
+    return accumulator;
   }
 
   /**
@@ -4566,6 +4618,27 @@
   /*--------------------------------------------------------------------------*/
 
   /**
+   * Creates a function that returns `value`.
+   *
+   * @static
+   * @memberOf _
+   * @category Utilities
+   * @param {*} value The value to return from the new function.
+   * @returns {Function} Returns the new function.
+   * @example
+   *
+   * var object = { 'name': 'fred' };
+   * var getter = _.constant(object);
+   * getter() === object;
+   * // => true
+   */
+  function constant(value) {
+    return function() {
+      return value;
+    };
+  }
+
+  /**
    * Produces a callback bound to an optional `thisArg`. If `func` is a property
    * name the created callback will return the property value for a given element.
    * If `func` is an object the created callback will return `true` for elements
@@ -5184,6 +5257,7 @@
   lodash.chain = chain;
   lodash.compact = compact;
   lodash.compose = compose;
+  lodash.constant = constant;
   lodash.countBy = countBy;
   lodash.debounce = debounce;
   lodash.defaults = defaults;
@@ -5221,6 +5295,7 @@
   lodash.throttle = throttle;
   lodash.times = times;
   lodash.toArray = toArray;
+  lodash.transform = transform;
   lodash.union = union;
   lodash.uniq = uniq;
   lodash.values = values;
